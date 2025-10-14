6a74258897307f756011aa414577bf7067d471d9
Lyle Jose <ljose2@cps.edu>
2025-10-14 10:13:56 -0500

    worked on function

--- main.py
+++ main.py
@@ -2,20 +2,41 @@ from crumbl import crumbl_db
 from match import match 
 from typing import List, Tuple, Callable, Any
 
-def get_monday_cookie():
-    pass
+def get_day(crumb):
+    return crumb[0]
 
-def get_tuesday_cookie():
-    pass
+def get_cookie(crumb):
+    return crumb[1]
 
-def get_wednesday_cookie():
-    pass
+def get_monday_cookie(matches):
+    day = matches[0]
+    result = []
+    for crumb in crumbl_db:
+        if get_day(crumb) == "Monday":
+            result.append(get_cookie(crumb))
+    return result
 
-def get_thursday_cookie():
-    pass
+def get_tuesday_cookie(matches):
+    day = matches[0]
 
-def get_friday_cookie():
-    pass
+def get_wednesday_cookie(matches):
+    day = matches[0]
 
-def get_saturday_cookie():
-    pass
+def get_thursday_cookie(matches):
+    day = matches[0]
+
+def get_friday_cookie(matches):
+    day = matches[0]
+
+def get_saturday_cookie(matches):
+    day = matches[0]
+
+def search_pa_list(src):
+    """
+    for pat, act in pa_list:
+        mat = match(pat, src)
+        if mat is not None:
+            answer = act(mat)
+            return answer if answer else ["No answers']"]
+    return ["I don't understand"]   
+    """
