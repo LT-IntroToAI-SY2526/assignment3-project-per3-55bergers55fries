50f1cd7d07cf26b8bb685da6941880c164715d8f
Lyle Jose <ljose2@cps.edu>
2025-10-14 10:01:51 -0500

    created functions

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 from crumbl import crumbl_db
 from match import match 
 from typing import List, Tuple, Callable, Any
+
+def get_monday_cookie():
+    pass
+
+def get_tuesday_cookie():
+    pass
+
+def get_wednesday_cookie():
+    pass
+
+def get_thursday_cookie():
+    pass
+
+def get_friday_cookie():
+    pass
+
+def get_saturday_cookie():
+    pass
