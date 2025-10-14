33c3f75fa2b53fd1f07d8ce4a02a7a24edd14c09
Aylish Irizarry <airizarry6@cps.edu>
2025-10-14 10:32:38 -0500

    added to database

--- crumbl_db.py
+++ crumbl_db.py
@@ -1,4 +1,33 @@
-from typing import List 
+from typing import List, Tuple 
 
 
-def match(pattern: List[str], source: List[str]) -> List[str]:
+crumbl_db: List[Tuple[str, List[str]]] = [
+    (
+        "Monday", # day of the weel
+        ["Dubai chocolate brownie"], # cookie
+    ),
+    (
+        "Tuesday",
+        ["Biscoff tres leches"],
+    ),
+    (
+        "Wednesday",
+        ["Semi sweet chocolate chip"],
+    ),
+    (
+        "Thursday",
+        ["Brownie sundae"],
+    ),
+    (
+        "Friday",
+        ["Tres leches cake"],
+    ),
+    (
+        "Saturday",
+        ["Cookies & cream"],
+    ),
+    (
+        "Sunday",
+        ["Crumble is closed!!!"],
+    ),
+]
