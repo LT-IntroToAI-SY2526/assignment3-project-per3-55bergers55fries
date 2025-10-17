4dd9627b0cde384ab8291af8a504f5810a7362d6
Lyle Jose <ljose2@cps.edu>
2025-10-17 09:58:47 -0500

    finished action functions

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def get_cookie_availability(matches):
     available = False
     result = []
     for crumb in crumbl_db:
-        if get_cookie(crumb) == cookie:
+        if cookie.lower() in get_cookie(crumb)[0].lower(): 
             available = True
             break
     if available:
@@ -46,11 +46,24 @@ def get_random_cookie(matches):
     rand = random.randint(0, 5)
     result.append(crumbl_db[rand][1])
     return result
+
+def get_cookie_day(matches):
+    cookie = matches[0]
+    result = []
+    for crumb in crumbl_db:
+        if cookie.lower() in get_cookie(crumb)[0].lower():
+            result.append(get_day(crumb))
+    if len(result) > 0:
+        return result
+    else:
+        return ["Sorry we don't have that cookie on any day"]
+    
 pa_list: List[Tuple[List[str], Callable[[List[str]], List[Any]]]] = [
     (str.split("what is the cookie lineup for %"), get_next_week_lineup),
     (str.split("what cookie do they serve on _"), get_day_cookie),
     (str.split("give me a random cookie"), get_random_cookie),
     (str.split("do you have %"), get_cookie_availability),
+    (str.split("what day do they serve %"), get_cookie_day)
 ]
 def search_pa_list(src):
     
